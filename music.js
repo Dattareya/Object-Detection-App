170adc2be74e03a62d84831aaa3c4f496f24cb2f
Dattareya <82696837+Dattareya@users.noreply.github.com>
2022-12-22 23:08:54 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,13 @@
+function setup()
+{
+   canvas=createCanvas(640,420);
+   canvas.center(); 
+
+   objectDetector=ml5.objectDetector("cocossd",modelloaded);
+   document.getElementById("status").innerHTML="Status:Detecting Objects";
+
+}
+function preload()
+{
+    img=loadImage("system.jpg");
+}
